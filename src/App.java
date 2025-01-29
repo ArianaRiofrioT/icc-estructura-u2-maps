72a3798ae78c785a32913a071ab9fd6ea2d3cfa2
Ariana Riofrio <ariofriot@est.ups.edu.ec>
2025-01-29 13:40:09 -0500

    MAPS

--- src/App.java
+++ src/App.java
@@ -7,26 +7,46 @@ import models.Empleado;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        // Ejecuta el ejemplo de uso de HashMap con ejemplos sencillos
-        runMapExamlpe();
-
-        // Ejecuta el ejemplo de gestión de empleados usando HashMap
         runEmpleadoExample();
 
-        // Ejecuta los ejercicios de sumatoria y anagramas
-        runEjerccios();
+    }
+
+    private static void runLinkedHashMapa() {
+        new Mapa().LinkedHashMapa();
+    }
+
+    private static void runTreeMapa() {
+        new Mapa().TreeMapa();
     }
 
     private static void runEmpleadoExample() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        EmpleadoContoller emCtrl = new EmpleadoContoller();
+        boolean result = emCtrl.addEmpleado(new Empleado(1, "Pablo", "Profesor"));
+        System.out.println("\nIngreso " + result );
+        result = emCtrl.addEmpleado(new Empleado(2, "Ariana", "Estudiante"));
+        System.out.println("Ingreso " + result );
+        
+        Empleado empleado = emCtrl.getEmpleadoById(1);
+        System.out.println("Empleado encontrado: " + empleado);
+        
+        System.out.println("Lista de empleados:");
+        emCtrl.displayEmpleados();
+
+        System.out.println("Lista de empleados solo por nombres:");
+        emCtrl.displayEmpleadosSoloNombres();
+
+        System.out.println("Lista de llaves de empleados:");
+        emCtrl.displayLlaves();
+
+        System.out.println("Lista de empleados (valores):");
+        emCtrl.displayValues();
+
     }
 
     private static void runMapExamlpe() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        new Mapa();
     }
 
     private static void runEjerccios() {
-        throw new UnsupportedOperationException("Not implemented yet");
-
     }
 }
